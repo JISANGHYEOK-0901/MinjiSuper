54969a0a322d9822c8c92d04ae5664dbb2ed4c66
Hyeok <wltkdgur123@naver.com>
2026-03-04 16:37:25 +0900

    어드민 페이지 개발

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import MainPage from "./pages/MainPage";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/minji_Admin" element={<AdminPage />} />
       </Routes>
     </Router>
   );
